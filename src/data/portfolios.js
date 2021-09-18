ffac0c66d28320e232be022768c42256fad87777
sirazul263 <sirazul263@gmail.com>
2021-09-18 18:51:54 +0600

    updated

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -7,6 +7,9 @@ import riders from "../img/portImages/riders.png";
 import dream from "../img/portImages/dream.png";
 import hotel from "../img/portImages/hotel.png";
 import web from "../img/portImages/web.png";
+import whatsapp from "../img/portImages/whatsapp.png";
+import dice from "../img/portImages/dice.png";
+import pinMatcher from "../img/portImages/pin-matcher.png";
 const portfolios = [
   {
     id: 1,
@@ -17,7 +20,51 @@ const portfolios = [
     title: "My Website",
     text: "Created with Styled Component, React, Node.js, Express.js and MongoDb",
   },
-
+  {
+    id: 11,
+    category: "React JS",
+    image: whatsapp,
+    link1: "https://github.com/sirazul263/whatsapp-clone-client",
+    link2: "https://whatsapp-79e18.web.app/",
+    title: "WhatsApp Clone",
+    text: "Created with Material UI, React, Socket Io, Node.js, Express.js and MongoDb",
+  },
+  {
+    id: 6,
+    category: "React JS",
+    image: doctor,
+    link1: "https://github.com/sirazul263/doctor-appointment-client",
+    link2: "https://doctors-portal-8fc86.firebaseapp.com/",
+    title: "Doctor's Portal",
+    text: "Doctor appointment system created with Bootstrap, React, Firebase, Node.js, Express.js and MongoDb",
+  },
+  {
+    id: 5,
+    category: "React JS",
+    image: ema,
+    link1: "https://github.com/sirazul263/simple-e-commerce",
+    link2: "https://amazon-clone-sirazul263.vercel.app/",
+    title: "Simple E-commerce",
+    text: "A Simple e-commerce created with Bootstrap, React, Firebase, Node.js, Express.js and MongoDb",
+  },
+  {
+    id: 13,
+    category: "Javascript",
+    image: dice,
+    link1: "https://github.com/sirazul263/dice-game",
+    link2: "https://sirazul263.github.io/dice-game/",
+    title: "Dice Game",
+    text: "Dice game created with vanilla JavaScript",
+  },
+  {
+    id: 14,
+    category: "Javascript",
+    image: pinMatcher,
+    link1: "https://github.com/sirazul263/pin-matcher",
+    link2: "https://sirazul263.github.io/pin-matcher/",
+    title: "Pin Matcher",
+    text: "Pin matcher created with vanilla JavaScript",
+  },
   {
     id: 2,
     category: "CSS",
@@ -36,24 +83,7 @@ const portfolios = [
     title: "Banking",
     text: "A simple banking system created with Bootstrap and JavaScript",
   },
-  {
-    id: 5,
-    category: "React JS",
-    image: ema,
-    link1: "https://github.com/sirazul263/simple-e-commerce",
-    link2: "https://amazon-clone-sirazul263.vercel.app/",
-    title: "Ema-John",
-    text: "A Simple E-Commerce created with Bootstrap, React and Firebase",
-  },
-  {
-    id: 6,
-    category: "React JS",
-    image: doctor,
-    link1: "https://github.com/sirazul263/doctor-appointment-client",
-    link2: "https://doctor-appointment-client.vercel.app/",
-    title: "Doctor's Portal",
-    text: "Doctor appointment system created with Bootstrap, React, Firebase, Node.js, Express.js and MongoDb",
-  },
+
   {
     id: 7,
     category: "React JS",
